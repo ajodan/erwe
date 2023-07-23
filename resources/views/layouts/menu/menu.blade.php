42eaf2c90ba6b8cc860e31c95f2f6dc4e44f3396
[ekselcorp] <ekselcorp@gmail.com>
2023-07-23 08:46:52 +0700

    Update 23 Juli 2023

--- resources/views/layouts/menu/menu.blade.php
+++ resources/views/layouts/menu/menu.blade.php
@@ -25,12 +25,12 @@
               <a class="dropdown-item " href="{{ route('strukturrt4') }}">RT 04</a>
             </div>
           </li>
-          <li class="nav-item">
+          {{-- <li class="nav-item">
             <a class="nav-link " href="{{ route('berita') }}">Berita</a>
           </li>
           <li class="nav-item">
             <a class="nav-link " href="{{ route('galeri') }}">Galeri</a>
-          </li>
+          </li> --}}
           {{-- <li class="nav-item">
             <a class="nav-link " href="blog-grid.html">Blog</a>
           </li> --}}
