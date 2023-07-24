2889f4d03e2af6ac7134f8a60e2e55686ee9d5ae
[ekselcorp] <ekselcorp@gmail.com>
2023-07-24 20:57:58 +0700

    update peta google kontak

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -43,7 +43,7 @@
           <div class="col-sm-12">
             <div class="contact-map box">
               <div id="map" class="contact-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.897134301598!2d107.04560164512681!3d-6.216941105736126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698f5ff2fd57bf%3A0x6a900bb292061610!2sEksel%20Corp!5e0!3m2!1sid!2sid!4v1688205201466!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.5893580957032!2d107.04511036963318!3d-6.216505666391729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698ed10d232fb3%3A0x6d006ac28fdcd367!2sJl.%20Perumahan%20Taman%20Alamanda%20No.9%2C%20Karangsatria%2C%20Kec.%20Tambun%20Utara%2C%20Kabupaten%20Bekasi%2C%20Jawa%20Barat%2017510!5e0!3m2!1sid!2sid!4v1690079919072!5m2!1sid!2sid" width="1300" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  </div>
               </div>
             </div>
           </div>
